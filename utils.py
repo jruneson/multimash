117e2f3d70f82b4d015e4b82a8fbff0a3f1d0512
Johan Runeson <johan.runeson@gmail.com>
2025-01-13 10:33:59 +0100

    Added input files and updated utils.py to use these inputs, and added references

--- utils.py
+++ utils.py
@@ -3,7 +3,10 @@ import numpy as np
 import argparse
 import sys
 from src import mashf90
+import os 
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+input_dir = script_dir + '/input'
 
 class CustomArgumentParser(argparse.ArgumentParser):
     """ Allow commented lines in input file """
@@ -19,14 +22,12 @@ kB = 3.166829e-6
 eV = 1./27.2113961
 
 """ Define global variables """
-Vconst = 0.; Vlin = 0.; Vbilin = 0.; nf_site = 0; ns = 0
-w_Ohm = 0.; c_Ohm = 0.; w_intra = 0.; c_intra = 0.
+Vconst = 0.; nf_site = 0; ns = 0
+w_low = 0.; c_low = 0.; w_intra = 0.; c_intra = 0.
 
 def read_args():
+    """ Run file with 'mash.py +input.in' """
     parser = CustomArgumentParser(fromfile_prefix_chars=["@","+"])
-    conv = parser.add_argument_group("convergence")
-    debye = parser.add_argument_group("debye")
-    tully = parser.add_argument_group("tully")
     parser.add_argument("-model", type=str, default="spinboson", help="Model system", 
                         choices=["spinboson","fmo3","fmo7","fmo8","tully1","lh2","lhc2"])
     parser.add_argument("-basis", type=str, default="site", help="Diabatic basis for certain model systems", 
@@ -39,29 +40,32 @@ def read_args():
                         choices=["pop","all","nuc"])
     parser.add_argument("-init", type=int,help="Initial state (in Python indexing)")
     parser.add_argument("-initbasis",type=str,default='dia',choices=["dia","adia","site","exc"],help='Basis for initial state')
-    parser.add_argument("-nucsamp",type=str,default='cl', help="Nuclear sampling (in case 'init' is not None). GS=ground state, WP=wavepacket",
-                        choices=['classical','cl','wigner','wig','GS','WP','clzero'])
+    parser.add_argument("-nucsamp",type=str,default='cl', 
+                        help="Nuclear sampling. classical/cl=classical, wigner/wig=thermal wigner, GS=ground state Wigner, clzero=classical T=0, WP=wavepacket",
+                        choices=['classical','cl','wigner','wig','GS','clzero','WP'])
     parser.add_argument("-elsamp",type=str,default='focused',choices=["focused","theta"],help='Choice of initial distribution')
-    parser.add_argument("-disorder",type=str,default='none',choices=["none","fmo","bSP","lhc2"],help='Choice of static disorder')
-    parser.add_argument("-bath",type=str,default='debye',choices=["debye","expt","B777"],help='Choice of bath')
-    parser.add_argument("-LHCmodel",type=str,default='Muh',choices=["Muh","Novo"],help='Choice of HS for LHC2')
-    parser.add_argument("-wmax",type=float,default=None,help="Max omega in discreziation")
     parser.add_argument("-debug",action="store_true",help='Toggle debug code (plot energy conservation etc)')
-    parser.add_argument("-polaron",type=str,choices=["langfirsov","variational","variationalsite"],help='Type of polaron transformation to remove high-frequency modes')
+    parser.add_argument("-disorder",type=str,default='none',choices=["none","fmo","lhc2"],help='Choice of static disorder')
+    parser.add_argument("-bath",type=str,default='debye',choices=["debye","coursegrain","B777"],help='Choice of low-frequency bath for fmo and lhc2')
+    parser.add_argument("-wmax",type=float,default=None,help="Max omega in discreziation")
+    parser.add_argument("-polaron",type=str,choices=["vpt","langfirsov"],help='Type of polaron transformation to remove high-frequency modes')
     parser.add_argument("-beta",type=float,default=1.,help="Reciprocal temperature [in a.u.]")
     parser.add_argument("-T",type=float,default=0,help="Temperature [in kelvin]")
-    conv.add_argument("-dt",default=41,type=float,help="Time step")
-    conv.add_argument("-nt","-TS",type=int,help="Number of time steps")
-    conv.add_argument("-ntraj","-traj",type=int,help="Number of trajectories")
-    conv.add_argument("-npar","-n",default=1,type=int,help="Number of parallel processors")
-    debye.add_argument("-Delta",type=float,default=1.,help="Diabatic coupling")
-    debye.add_argument("-epsilon","-eps",type=float,default=0.,help="(Half) energy bias")
-    debye.add_argument("-lamda",type=float,default=0,help="System-bath reorganization energy.")
-    debye.add_argument("-omegac",type=float,default=0.,help="Cutoff frequency.")
-    debye.add_argument("-nf",type=int,default=1,help="Number of bath modes.")
-    tully.add_argument("-WPenergy",type=float,default=0.,help="Wavepacket energy")
-    tully.add_argument("-gamma",type=float,default=0.,help="Wavepacket width parameter")
-    tully.add_argument("-pinit",type=float,default=0.,help="Initial momentum")
+    # Convergence
+    parser.add_argument("-dt",default=41,type=float,help="Time step")
+    parser.add_argument("-nt","-TS",type=int,help="Number of time steps")
+    parser.add_argument("-ntraj","-traj",type=int,help="Number of trajectories")
+    parser.add_argument("-nf",type=int,default=1,help="Number of bath modes.")
+    parser.add_argument("-npar","-n",default=1,type=int,help="Number of parallel processors")
+    # Spin-boson model
+    parser.add_argument("-Delta",type=float,default=1.,help="Diabatic coupling")
+    parser.add_argument("-epsilon","-eps",type=float,default=0.,help="(Half) energy bias")
+    parser.add_argument("-lamda",type=float,default=0,help="System-bath reorganization energy.")
+    parser.add_argument("-omegac",type=float,default=0.,help="Cutoff frequency.")
+    # Tully model
+    parser.add_argument("-WPenergy",type=float,default=0.,help="Wavepacket energy")
+    parser.add_argument("-gamma",type=float,default=0.,help="Wavepacket width parameter")
+    parser.add_argument("-pinit",type=float,default=0.,help="Initial momentum")
     args = parser.parse_args()
 
     """ ======= Convert input arguments to atomic units ======="""
@@ -81,7 +85,7 @@ def read_args():
 
 """======== Initialize system ========="""
 def setup_model(args):
-    global Vconst, Vlin, Vbilin, nf_site, w_Ohm, c_Ohm, w_intra, c_intra, ns
+    global Vconst, nf_site, w_low, c_low, w_intra, c_intra, ns
     model = args.model
     epsilon = args.epsilon
     Delta = args.Delta
@@ -107,7 +111,6 @@ def setup_model(args):
         Vconst[0,2] =   5.5
         Vconst[1,2] =  30.8
         Vconst = Vconst + Vconst.T - np.diag(np.diag(Vconst))
-        # Vconst -= np.eye(ns)*np.min(np.diag(Vconst))
         Vconst -= np.eye(ns)*np.trace(Vconst)/ns
 
         Vconst = Vconst*cmm1
@@ -119,10 +122,10 @@ def setup_model(args):
             c[i] = fac*omega[i]
         for i in range(ns):
             omega[i*nf_site:(i+1)*nf_site] = omega[:nf_site]
-            Vlin[i*nf_site:(i+1)*nf_site,i,i] = -c
+            Vlin[i*nf_site:(i+1)*nf_site,i,i] = c
+
     elif model=='fmo7':
         ns = 7
-        # Vlin = np.zeros((nf,ns,ns),dtype=np.float64)
         omega = np.zeros(nf,dtype=np.float64)
         Vconst = np.diag([12410.,12530,12210,12320,12480,12630,12440])
         Vconst[0,1] = -87.7
@@ -152,136 +155,80 @@ def setup_model(args):
 
     elif model=="fmo8":
         ns = 8
-        Vlin = np.zeros((nf,ns,ns),dtype=np.float64)
+        """ Vconst from Schmidt am Busch, Müh, Madjet and Renger JPCL 2, 93 (2011) """
+        Vconst = np.loadtxt(input_dir+'/FMO-HS.dat') * cmm1
         omega = np.zeros(nf,dtype=np.float64)
-        Vconst = np.diag([12505.,12425,12195,12375,12600,12515,12465,12700])
-        Vconst[0,1] = -94.8
-        Vconst[0,2] =   5.5
-        Vconst[0,3] = - 5.9
-        Vconst[0,4] =   7.1
-        Vconst[0,5] = -15.1
-        Vconst[0,6] = -12.2
-        Vconst[0,7] =  39.5
-        Vconst[1,2] =  29.8
-        Vconst[1,3] =   7.6
-        Vconst[1,4] =   1.6
-        Vconst[1,5] =  13.1
-        Vconst[1,6] =   5.7
-        Vconst[1,7] =   7.9
-        Vconst[2,3] = -58.9
-        Vconst[2,4] = - 1.2
-        Vconst[2,5] =  -9.3
-        Vconst[2,6] =   3.4
-        Vconst[2,7] =   1.4
-        Vconst[3,4] = -64.1
-        Vconst[3,5] = -17.4
-        Vconst[3,6] = -62.3
-        Vconst[3,7] = - 1.6
-        Vconst[4,5] =  89.5
-        Vconst[4,6] = - 4.6
-        Vconst[4,7] =   4.4
-        Vconst[5,6] =  35.1
-        Vconst[5,7] = - 9.1
-        Vconst[6,7] = -11.1
-        Vconst = Vconst + Vconst.T - np.diag(np.diag(Vconst))
-        Vconst -= np.eye(ns)*np.min(np.diag(Vconst))
-        Vconst = Vconst*cmm1
 
     elif model=='lh2':
         ns = 24
         """ Ordering as follows: 
-        ------B850-----------   ---B800----
+        --------B850---------   ---B800----
         1A 1B 2A 2B ... 8A 8B   1 2 3 ... 8
         """
-        """ Load system Hamiltonian """
-        Vconst = np.loadtxt('HS.in') * cmm1
+        """ Load system Hamiltonian: Tretiak, Middleton, Chernyak, Mukamel JPCB 104, 9540 (2000) """
+        Vconst = np.loadtxt(input_dir+'/LH2-HS.dat') * cmm1
 
-        """ Load bath """
-        specden = np.loadtxt('specden.in')
+        """ Load discrete bath: Rätsep, Cai, Reimers, Freiberg JCP 134, 024506 (2011) """
+        specden = np.loadtxt(input_dir+'/LH2-specden.dat')
         w_intra = specden[:,0] * cmm1
         S_HR = specden[:,1]/1000.
-        # lamda = specden[:,2] * cmm1
         c_intra = np.sqrt(2.*w_intra**3*S_HR)
         Jw = np.pi/2. * c_intra**2/w_intra
-        nf_intra = len(w_intra)
         lamda_intra = np.sum(Jw/w_intra)/np.pi
         print('lambda (intra): ', lamda_intra/cmm1)
         
         """ Ohmic part: J(w) = eta*w*exp(-w/wc)"""
         xi = 0.4
-        eta = 2*np.pi*xi # 2*pi/s1^2 = 1
+        eta = 2*np.pi*xi 
         omegac = 200.*cmm1
         nf_site = args.nf
-        w_Ohm = np.zeros(nf_site)
-        c_Ohm = np.zeros(nf_site)
+        w_low = np.zeros(nf_site)
+        c_low = np.zeros(nf_site)
         fac = np.sqrt(2*eta*omegac/(nf_site*np.pi))
         for i in range(nf_site):
-            w_Ohm[i] = -omegac*np.log((i+0.5)/nf_site)
-            c_Ohm[i] = fac*w_Ohm[i]
-        Jw_Ohm = np.pi/2.*c_Ohm**2/w_Ohm
-        lamda_Ohm = np.sum(Jw_Ohm/w_Ohm)/np.pi
+            w_low[i] = -omegac*np.log((i+0.5)/nf_site)
+            c_low[i] = fac*w_low[i]
+        Jw_Ohm = np.pi/2.*c_low**2/w_low
+        lamda_Ohm = np.sum(Jw_Ohm/w_low)/np.pi
         print('lambda (Ohmic): ', lamda_Ohm/cmm1)
 
     elif model=='lhc2':
         ns = 14
-        """ Load system Hamiltonian (Choose between Muh and Novo=Novoderezhkin) """
-        Vconst = np.loadtxt('HS_%s.in'%args.LHCmodel) * cmm1
+        """ Load system Hamiltonian: couplings from Müh, Madjet and Renger JPCB 114, 13517 (2010).
+         Site energies from Müh and Renger Biochim. Biophys. Acta 1817, 1446 (2012). """
+        Vconst = np.loadtxt(input_dir+'/LHCII-HS.dat') * cmm1
         E,U = np.linalg.eigh(Vconst)
         print(U[args.init,:]**2)
 
-        """ Load bath """
-        if args.bath=='debye':
-            nf_site = nf
-            lamda = 220 * cmm1
-            omegac = 353.7 * cmm1
-            fac = np.sqrt(2*lamda/nf_site)
-            c = np.zeros(nf_site)
-            omega_site = np.zeros(nf_site)
-            for i in range(nf_site):
-                omega_site[i] = omegac*np.tan(0.5*np.pi*(i+0.5)/nf_site)
-                c[i] = fac*omega_site[i]
-        elif args.bath in ['expt','B777']:
-            specden = np.loadtxt('specden.in')
-            w_intra = specden[:,0] * cmm1
-            S_HR = specden[:,1]
-            c_intra = np.sqrt(2.*w_intra**3*S_HR)
-            nf_intra = len(w_intra)
+        """ Load discrete bath: Renger, Madjet, Knorr and Müh J. Plant Physiol. 168, 1497 (2011) """
+        specden = np.loadtxt(input_dir+'/LHCII-specden.dat')
+        w_intra = specden[:,0] * cmm1
+        S_HR = specden[:,1]
+        c_intra = np.sqrt(2.*w_intra**3*S_HR)
 
-            if args.bath=='expt':
-                """ Low-frequency bath: Debye spectral density """
-                lamda = 37.*cmm1
-                omegac = 30.*cmm1
-                nf_site = args.nf
-                fac = np.sqrt(2*lamda/nf_site)
-                w_Ohm = np.zeros(nf_site)
-                c_Ohm = np.zeros(nf_site)
-                for i in range(nf_site):
-                    w_Ohm[i] = omegac*np.tan(0.5*np.pi*(i+0.5)/nf_site)
-                    c_Ohm[i] = fac*w_Ohm[i]
-            elif args.bath=='B777':
-                """ Low-frequency bath: Renger et al. J. Plant Physiol. 2011, 168, 1497 """
-                S0 = 0.5
-                w1 = 0.56 * cmm1
-                w2 = 1.94 * cmm1
-                s1 = 0.8
-                s2 = 0.5
-                def Jw_term(w,wi,si):
-                    return si/(2*np.math.factorial(7)*wi**4)*w**3*np.exp(-np.sqrt(w/wi))
-                def Jw_l(w):
-                    return S0/(s1+s2)*(Jw_term(w,w1,s1) + Jw_term(w,w2,s2))
+        """ Low-frequency bath: Renger et al. J. Plant Physiol. 168, 1497 (2011) """
+        S0 = 0.5
+        w1 = 0.56 * cmm1
+        w2 = 1.94 * cmm1
+        s1 = 0.8
+        s2 = 0.5
+        def Jw_term(w,wi,si):
+            return si/(2*np.math.factorial(7)*wi**4)*w**3*np.exp(-np.sqrt(w/wi))
+        def Jw_l(w):
+            return S0/(s1+s2)*(Jw_term(w,w1,s1) + Jw_term(w,w2,s2))
 
-                maxw = args.wmax * cmm1
-                nf_site = nf
-                w_Ohm = maxw * np.arange(1,nf+1)/nf
-                Jw_disc = Jw_l(w_Ohm)
-                dw = w_Ohm[1]-w_Ohm[0]
-                g_Ohm = np.sqrt(Jw_disc*dw)
-                c_Ohm = g_Ohm * np.sqrt(2*w_Ohm**3)
+        maxw = args.wmax * cmm1
+        nf_site = nf
+        w_low = maxw * np.arange(1,nf+1)/nf
+        Jw_disc = Jw_l(w_low)
+        dw = w_low[1]-w_low[0]
+        g_Ohm = np.sqrt(Jw_disc*dw)
+        c_low = g_Ohm * np.sqrt(2*w_low**3)
 
-            lamda = 0.5*np.sum(c_Ohm**2/w_Ohm**2)
-            print(lamda/cmm1)
-            lamda_intra = 0.5*np.sum(c_intra**2/w_intra**2)
-            print(lamda_intra/cmm1)
+        lamda = 0.5*np.sum(c_low**2/w_low**2)
+        print(lamda/cmm1)
+        lamda_intra = 0.5*np.sum(c_intra**2/w_intra**2)
+        print(lamda_intra/cmm1)
 
     if 'fmo' in model:
         if args.bath=='debye':
@@ -295,15 +242,16 @@ def setup_model(args):
             for i in range(ns):
                 omega[i*nf_site:(i+1)*nf_site] = omega[:nf_site]
                 Vlin[i*nf_site:(i+1)*nf_site,i,i] = c
-        elif args.bath=='expt':
-            """ Course-grained Huang-Rhys factors for low-frequency modes """
-            specden = np.loadtxt('specden_low.in')
-            w_Ohm = specden[:,0] * cmm1
+        elif args.bath=='coursegrain':
+            """ Course-grained Huang-Rhys factors for low-frequency modes.
+             Based on raw data from Klinger, Lindorfer, Müh and Renger JCP 153, 215103 (2020) """
+            specden = np.loadtxt(input_dir+'/FMO-specden_coursegrain.dat')
+            w_low = specden[:,0] * cmm1
             S_HR = specden[:,1]
-            c_Ohm = np.sqrt(2.*w_Ohm**3*S_HR)
-            nf_site = len(w_Ohm)
-
+            c_low = np.sqrt(2.*w_low**3*S_HR)
+            nf_site = len(w_low)
         elif args.bath=='B777':
+            """ Adolphs and Renger, Biophys J. 91, 2778 (2006) """
             S0 = 0.5
             w1 = 0.56 * cmm1
             w2 = 1.94 * cmm1
@@ -313,33 +261,30 @@ def setup_model(args):
                 return si/(2*np.math.factorial(7)*wi**4)*w**3*np.exp(-np.sqrt(w/wi))
             def Jw_l(w):
                 return S0/(s1+s2)*(Jw_term(w,w1,s1) + Jw_term(w,w2,s2))
-
             maxw = args.wmax * cmm1
             nf_site = nf
-            w_Ohm = maxw * np.arange(1,nf+1)/nf
-            Jw_disc = Jw_l(w_Ohm)
-            dw = w_Ohm[1]-w_Ohm[0]
+            w_low = maxw * np.arange(1,nf+1)/nf
+            Jw_disc = Jw_l(w_low)
+            dw = w_low[1]-w_low[0]
             g_Ohm = np.sqrt(Jw_disc*dw)
-            c_Ohm = g_Ohm * np.sqrt(2*w_Ohm**3)
+            c_low = g_Ohm * np.sqrt(2*w_low**3)
 
-        if args.bath in ['expt','B777']:
-            """ Load experimental spectral density for high-frequency modes:
-                Wendling et al. JPCB 2000, 104, 5825 for Prostechochloris aestuarii
-                Raetsep and Freiberg J. Lumin. 2007, 127, 251 for Cb. tepidum """
-            specden = np.loadtxt('specden.in')
-            w_intra = specden[:,0] * cmm1
-            S_HR = specden[:,1]
-            c_intra = np.sqrt(2.*w_intra**3*S_HR)
-            nf_intra = len(w_intra)
+    if model=='fmo8':
+        """ Load experimental spectral density for high-frequency modes:
+            Wendling et al. JPCB 2000, 104, 5825 for Prostechochloris aestuarii """
+        specden = np.loadtxt(input_dir+'/FMO-specden.dat')
+        w_intra = specden[:,0] * cmm1
+        S_HR = specden[:,1]
+        c_intra = np.sqrt(2.*w_intra**3*S_HR)
 
-            lamda = 0.5*np.sum(c_Ohm**2/w_Ohm**2)
-            print(lamda/cmm1)
-            lamda_intra = 0.5*np.sum(c_intra**2/w_intra**2)
-            print(lamda_intra/cmm1)
+        lamda = 0.5*np.sum(c_low**2/w_low**2)
+        print(lamda/cmm1)
+        lamda_intra = 0.5*np.sum(c_intra**2/w_intra**2)
+        print(lamda_intra/cmm1)
 
 
     if model in ['fmo8','lh2','lhc2']:
-        w,c,Vconst_renorm = polaron_transform(w_Ohm,w_intra,c_Ohm,c_intra,Vconst,args)
+        w,c,Vconst_renorm = polaron_transform(w_low,w_intra,c_low,c_intra,Vconst,args)
             
         """ Total vibronic part of Hamiltonian """
         nf_site = len(w)
@@ -363,7 +308,6 @@ def setup_model(args):
         if args.polaron is None:        
             mashf90.init_frexc(mass,omega,Vconst,kappa,nf,nf_site,ns)
         else:
-            print(mass.shape, omega.shape, kappa.shape, nf_site)
             mashf90.init_frexc(mass,omega,Vconst_renorm,kappa,nf,nf_site,ns)
     elif model in ['spinboson']:
         mashf90.init_linvib(mass,omega,Vconst,Vlin,nf,ns)
@@ -399,8 +343,8 @@ def polaron_transform(w_Ohm,w_intra,c_Ohm,c_intra,Vconst,args):
     coth = 2*nBE + 1
      
     if args.polaron=='langfirsov':
-        # Scale all off-diagonal potential by a band narrowing factor
-        # based on a Lang-Firsov polaron transformation of the high-
+        # Scale all off-diagonal potential by the same band narrowing factor
+        # based on a full Lang-Firsov polaron transformation of the high-
         # frequency intramolecular modes
         S_HR = g**2
         f = np.exp(-np.sum(S_HR*coth))
@@ -450,7 +394,9 @@ def polaron_transform(w_Ohm,w_intra,c_Ohm,c_intra,Vconst,args):
         avg /= ns*(ns-1)/2
         print('bandnarrow avg',avg)
         print('bandnarrow min',min)
-        return w,c,Hel
+    else:
+        sys.exit('args.polaron='+str(args.polaron)+' is not a valid option')
+    return w,c,Hel
 
 """ Debugging section to check energy conservation, plot adiabatic populations etc. """
 def debug(args,mass,omega,nf,ns):
@@ -514,7 +460,7 @@ def sample(args,mass,omega,nf,ns):
         newE = np.random.normal(np.diag(Vconst), E_sig)
         np.fill_diagonal(Vconst_dis, newE)
         if args.model in ['fmo8','lh2','lhc2']:
-            w,c,Vconst_dis = polaron_transform(w_Ohm,w_intra,c_Ohm,c_intra,Vconst_dis,args)
+            w,c,Vconst_dis = polaron_transform(w_low,w_intra,c_low,c_intra,Vconst_dis,args)
         kappa = np.zeros((nf_site,ns))
         for n in range(ns):
             omega[n*nf_site:(n+1)*nf_site] = w
@@ -524,7 +470,7 @@ def sample(args,mass,omega,nf,ns):
     for j in range(npar):
         """ Nuclear sampling """
         qmin = 0
-        if args.nucsamp in ['cl']:
+        if args.nucsamp in ['classical','cl']:
             """ Classical """
             q[:,j] = np.random.normal(qmin,1./np.sqrt(beta*mass*omega**2),nf)
             p[:,j] = np.random.normal(0,np.sqrt(mass/beta),nf)
